4bf18dc9f1f7a1c7741fe0de3689069c9e603ec7
yuanhengdai <yuanhengdai@gmail.com>
2024-04-11 16:37:14 +1000

    remove step3_remove_duplicate

--- download_TCGA_data/step3_remove_duplicate.R
+++ download_TCGA_data/step3_remove_duplicate.R
@@ -14,30 +14,51 @@ library(SummarizedExperiment)
 library(tidyverse)
 
 # 设置工作路径变量
-data_path <- "C:/D/PHD/bioinformatics/TCGA data/"
-file <- "GDCdata/TCGA-KIRC/KIRC_exp_fpkm_protein_coding.csv"
+setwd("C:/D/PHD/bioinformatics/TCGA data/")
+file <- "GDCdata/TCGA-KIRC/expr_fpkm_protein_coding.csv"
 
-
-file_protein_coding_fpkm <- "KIRC_exp_fpkm_protein_coding.csv"
-file_lncRNA_fpkm <- "KIRC_exp_fpkm_lncRNA.csv"
-
-# 使用变量设置工作路径
-setwd(data_path)
-
-#读取下载的表达矩阵,TCGA数据的文件名（XXXX需要自行修改）
-raw_file <- "raw_data/KIRC_expr_fpkm_protein_coding.csv"
-rawdata <- read.csv(raw_file, check.names = FALSE)
+# 读取下载的表达矩阵, TCGA数据的文件名（XXXX需要自行修改）
+df <- read.csv(file, check.names = FALSE)
 
 # 将第一列命名为 'Ensembl_ID'
-colnames(rawdata)[1] <- "Ensembl_ID"
+colnames(df)[1] <- "Ensembl_ID"
 
-suppressPackageStartupMessages(library(tidyverse))
+# 保留列名的前16个字符
+colnames(df) <- substr(colnames(df), 1, 16)
 
-expr_read <- file_protein_coding_fpkm %>% 
-  as_tibble() %>% # tibble不支持row name，我竟然才发现！
-  mutate(meanrow = rowMeans(.[,-1]), .before=2) %>% 
-  arrange(desc(meanrow)) %>% 
-  distinct(symbol_mrna,.keep_all=T) %>% 
-  select(-meanrow) %>% 
-  column_to_rownames(var = "symbol_mrna") %>% 
-  as.data.frame()
+# 查找重复的列名
+dup_cols <- colnames(df)[duplicated(colnames(df)) | duplicated(colnames(df), fromLast = TRUE)]
+
+# 初始化一个空数据框用于存储被删除的列
+deleted_columns <- data.frame()
+
+# 对于每一个重复的列名，保留平均值较大的列，删除较小的列
+for (col_name in unique(dup_cols)) {
+  # 获取所有重复列的索引
+  cols <- which(colnames(df) == col_name)
+  
+  # 如果只有一个列，不做处理
+  if (length(cols) <= 1) next
+  
+  # 计算每个重复列的平均值，并保留平均值较大的列
+  max_col <- cols[which.max(sapply(cols, function(i) mean(df[[i]], na.rm = TRUE)))]
+  
+  # 要删除的列
+  cols_to_delete <- setdiff(cols, max_col)
+  
+  # 添加要删除的列到deleted_columns数据框
+  deleted_columns <- rbind(deleted_columns, df[, cols_to_delete, drop = FALSE])
+  
+  # 删除较小的列
+  df <- df[, -cols_to_delete]
+}
+
+# 输出被删除的列
+if (nrow(deleted_columns) > 0) {
+  print("以下列被删除:")
+  print(colnames(deleted_columns))
+} else {
+  print("没有列被删除。")
+}
+
+
---
+++ download_TCGA_data/step3_remove_duplicate.py
@@ -0,0 +1,59 @@
+#date: 20240411
+#revesion: 1.0
+#author: Yuanheng 
+#email:Yuanheng.Dai@uon.edu.au
+#function: remove duplicate sample for every patient
+
+import pandas as pd
+import os
+
+# 设置工作目录
+work_path = 'C:/D/PHD/bioinformatics/TCGA data/GDCdata/TCGA-KIRC'
+os.chdir(work_path)
+
+# 输入输出文件名
+file_name = "KIRC_exp_fpkm_protein_coding.csv"
+output_file_name = 'filtered_KIRC_exp_fpkm_protein_coding.csv'
+
+# 读取CSV文件到DataFrame
+df = pd.read_csv(file_name)
+
+# 获取原始列名并截断前16个字符作为新列名
+original_columns = df.columns
+truncated_columns = [col[:16] for col in original_columns]
+
+# 创建一个映射关系，将截断的列名映射回它们的原始列名
+column_mapping = {}
+for original, truncated in zip(original_columns, truncated_columns):
+    column_mapping.setdefault(truncated, []).append(original)
+
+# 保存将要删除的列名到一个列表中
+columns_to_drop = []
+
+# 遍历映射关系，处理重复的列
+for truncated, originals in column_mapping.items():
+    if len(originals) > 1:  # 仅处理重复的列
+        # 计算每个原始列的平均值，并保留平均值最大的列
+        max_mean_col = max(originals, key=lambda col: df[col].mean())
+        columns_to_drop.extend(set(originals) - {max_mean_col})
+        
+        # 打印被删除的列名
+        print(f"Deleted columns for {truncated}: {set(originals) - {max_mean_col}}")
+
+# 删除列
+df.drop(columns_to_drop, axis=1, inplace=True)
+
+# 确保DataFrame使用更新后的列名
+df.columns = [column_mapping.get(col[:16], [col])[0] for col in df.columns]
+
+
+# 保存处理后的DataFrame到新的CSV文件
+df.to_csv(output_file_name, index=False)
+
+# 将删除的列名保存到另外一个文件
+deleted_columns_file = 'deleted_sample.txt'
+with open(deleted_columns_file, 'w') as f:
+    f.write('\n'.join(columns_to_drop))
+
+
+
